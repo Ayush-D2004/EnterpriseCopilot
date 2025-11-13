119021562d14a452f696e32a26cee05f0898123b
Ayush Dhoble <ayush.dhoble2004@gmail.com>
2025-11-13 19:38:45 +0530

    ~

--- webapp/ingest.py
+++ webapp/ingest.py
@@ -2,7 +2,7 @@
 import os
 import logging
 from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from huggingface_hub import login
